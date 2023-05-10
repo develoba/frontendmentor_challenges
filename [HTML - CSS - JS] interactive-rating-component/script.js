6c9fa2861e31fe9d2c43242f1f9a92a5e3b99bdb
elenaeslin <srta.scobar@gmail.com>
2023-05-10 10:13:45 +0200

    complete javascript fcuntion

--- [HTML - CSS - JS] interactive-rating-component/script.js
+++ [HTML - CSS - JS] interactive-rating-component/script.js
@@ -1,2 +1,22 @@
-let rate = document.getElementById("one").checked;
-console.log(rate)
+
+
+let btn = document.querySelector(".btn");
+let rates = document.getElementsByName("rate");
+const result = document.querySelector("#rate-result");
+const rateCard = document.querySelector("#rateCard");
+const resultCard = document.querySelector("#resultCard");
+
+console.log(rates);
+console.log(btn);
+console.log(result.innerHTML)
+
+btn.addEventListener('click', () => {
+    for(let i = 0; i < rates.length; i++) {
+        if(rates[i].checked) {
+            rateCard.classList.add('inactive');
+            resultCard.classList.remove('inactive');
+            result.innerHTML = rates[i].value;
+        }
+    }
+});
+
